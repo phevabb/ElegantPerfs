32de64209dedfa86c17222705b44e968c5645715
phevabb <phevab1@gmail.com>
2024-01-08 13:40:32 +0000

    3rd

--- store/admin.py
+++ store/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'price', 'old_price', 'category',  'is_available')
-    list_editable = ('price', 'old_price',)
+    list_display = ('product_name', 'price', 'old_price', 'category',  'is_available', 'stock')
+    list_editable = ('price', 'old_price', 'stock')
     prepopulated_fields = {'slug': ('product_name',)}
 
 # Register your models here.
