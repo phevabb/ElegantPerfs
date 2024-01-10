a284a594fd5cb03ae37a8343f66e263aa2d775b7
phevabb <phevab1@gmail.com>
2024-01-10 15:06:01 +0000

    search

--- store/views.py
+++ store/views.py
@@ -4,13 +4,13 @@ from category.models import Category
 from carts.views import _cart_id
 from carts .models import CartItem
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from django.db.models import Q
 
 
 
 # Create your views here.
 
-def store(request, category_slug=None):
+def store(request, category_slug=None): 
     categories = None
     products = None
          
@@ -51,3 +51,24 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'product_detail.html', context) 
+
+
+def search(request):
+    products = []
+    product_count = 0
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products=Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) |Q(product_name__icontains=keyword))
+            product_count = products.count()
+        else:
+            print("Nothing searched")
+            
+    context = {
+        'products': products,
+        'product_count':product_count
+
+
+    }
+    return render(request, 'store.html', context  )
